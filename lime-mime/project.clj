067fd6e063229138986682644411fbac7eaa3ddc
jeremyheiler <jeremyheiler@gmail.com>
2012-03-07 23:19:50 -0500

    Update copyright date in LICENSE.

--- lime-mime/project.clj
+++ lime-mime/project.clj
@@ -1,8 +1,6 @@
 (defproject lime-mime "0.1.0-SNAPSHOT"
   :description "A library for using MIME with Clojure."
   :url "http://limemail.org"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :main lime.mime)
 
